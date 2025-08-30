93932108eefd9efe8419d7dcb2ee0d4ac2f0fb74
Roman Serdiuk <roman.srdk21@gmail.com>
2025-08-30 14:04:40 -0400

    DONE

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'node:path';
+// const pathToWorkDir = path.join(process.cwd());
+// отримуємо шлях до кореневої директорії викликом метода process.cwd()
+// export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
+
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
